301e954d3a245827bcb25bc1208858409ff160ca
vijay-prema <vjprema@gmail.com>
2025-11-27 11:04:00 +1300

    Fix dependency issue

--- nanopub-utils/rollup.config.js
+++ nanopub-utils/rollup.config.js
@@ -3,7 +3,7 @@ import {nodeResolve} from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import nodePolyfills from 'rollup-plugin-polyfill-node'
-import {terser} from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser';
 
 const rollupConf = {
   input: 'src/index.ts',
